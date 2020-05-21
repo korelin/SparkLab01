46fa9ca3617c8b97fddfbcad2b520827cd091d4a
Ivan <korelin.ivan@gmail.com>
2020-05-21 20:54:34 +0500

    some improvements

--- src/main/scala/Lab01.scala
+++ src/main/scala/Lab01.scala
@@ -6,13 +6,14 @@ import java.io.PrintWriter
 case class Histogram(hist_film: Seq[Int], hist_all: Seq[Int])
 
 object Lab01 extends App{
-  def orderBy(x:Map[Int,Int]) = x.toSeq.sortBy(_._1).map(_._2)
+  def orderBy(x:Map[Int,Int]):Seq[Int]= x.toSeq.sortBy(_._1).map(_._2)
+
   val data = Source.fromFile("u.data")
   val records = (for (line <- data.getLines) yield (line.split("\t")(1) toInt, line.split("\t")(2) toInt)) toList
-  val itemsRaiting = records.groupBy(_._1) //first group by
+  val itemsRaiting = records groupBy(_._1) //first group by
   val allItemsRaiting = for ((k,v) <- itemsRaiting) yield (k, v.groupBy(_._2).mapValues(_.size)) //second group by
-  val totalRaitings = (allItemsRaiting toList) map(_._2) flatten
-  val total = totalRaitings.groupBy(_._1) mapValues(_.map(_._2).sum)
+  val totalRaitings = (allItemsRaiting toList) flatMap(_._2)
+  val total = totalRaitings groupBy(_._1) mapValues(_.map(_._2).sum) //third group by
   val hist = Histogram(orderBy(allItemsRaiting.get(64).get), orderBy(total))
   new PrintWriter("lab01.json") { write(hist.asJson.toString); close }
 }
